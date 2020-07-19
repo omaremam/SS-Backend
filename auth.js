e2a7f342a076ec1f57669d392923a92238a0c5d7
Ahmed A.Hamza <ahmedhamza96@gmail.com>
2020-07-19 23:26:54 +0200

    TestCommit

--- auth.js
+++ auth.js
@@ -18,8 +18,7 @@ router.post('/', async(req, res)=>{
     
     const validpassword= await bcrypt.compare(req.body.password, user.password);
     if(!validpassword) return res.status(400).send('Invalid email or password');
-    //res.status(200).send(user);
-    res.status(200).send(true);
+    res.status(200).send(user);
     // check for user auth pass
     });
     function validate(req){
