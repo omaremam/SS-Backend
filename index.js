554ab44e333dc91b046e5685a9d9a0c4dbc7997b
omaremam <omar.emam89@yahoo.com>
2020-08-26 15:59:40 +0200

    changed database

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
-mongoose.connect('mongodb://raghda:Gho0nima@cluster0-shard-00-00.1b2jk.mongodb.net:27017,cluster0-shard-00-01.1b2jk.mongodb.net:27017,cluster0-shard-00-02.1b2jk.mongodb.net:27017/test?authSource=admin&replicaSet=atlas-nbrdp5-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
+mongoose.connect('mongodb+srv://sharmelshiikh:sharm2020@ss-cluser.93027.mongodb.net/test?authSource=admin&replicaSet=atlas-jn82x6-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
     // this will create database in mongodb
     .then(() => console.log('connected to mongodb'))
     .catch(err => console.error('not connected to monogodb'));
